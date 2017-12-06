5a8d05c565e9710898870e6b1c11ffd6066a0651
0xFEEDC0DE64 <0xFEEDC0DE64@gmail.com>
2017-12-07 00:30:03 +0100

    UpdateDialog now starts GET request to GitHub API

--- dialogs/updatedialog.h
+++ dialogs/updatedialog.h
@@ -4,6 +4,7 @@
 #include <QDialog>
 
 class QNetworkAccessManager;
+class QNetworkReply;
 
 class ZeiterfassungSettings;
 namespace Ui { class UpdateDialog; }
@@ -16,9 +17,13 @@ public:
     explicit UpdateDialog(ZeiterfassungSettings &settings, QNetworkAccessManager *manager, QWidget *parent = 0);
     ~UpdateDialog();
 
+private Q_SLOTS:
+    void finished();
+
 private:
     Ui::UpdateDialog *ui;
     ZeiterfassungSettings &m_settings;
+    QNetworkReply *m_reply;
 };
 
 #endif // UPDATEDIALOG_H
