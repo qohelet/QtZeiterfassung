b9e40019a3e53307cca2a8de2c12d0044bd82681
0xFEEDC0DE64 <0xFEEDC0DE64@gmail.com>
2017-12-07 00:30:03 +0100

    UpdateDialog now starts GET request to GitHub API

--- dialogs/updatedialog.cpp
+++ dialogs/updatedialog.cpp
@@ -1,15 +1,34 @@
 #include "updatedialog.h"
 #include "ui_updatedialog.h"
 
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QDebug>
+
 UpdateDialog::UpdateDialog(ZeiterfassungSettings &settings, QNetworkAccessManager *manager, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::UpdateDialog),
     m_settings(settings)
 {
     ui->setupUi(this);
+
+    m_reply = manager->get(QNetworkRequest(QUrl(QStringLiteral("https://api.github.com/repos/0xFEEDC0DE64/QtZeiterfassung/releases"))));
+    connect(m_reply, &QNetworkReply::finished, this, &UpdateDialog::finished);
 }
 
 UpdateDialog::~UpdateDialog()
 {
     delete ui;
+}
+
+void UpdateDialog::finished()
+{
+    if(m_reply->error() != QNetworkReply::NoError)
+    {
+        qWarning() << m_reply->error() << m_reply->errorString();
+        return;
+    }
+
+    qDebug() << m_reply->readAll();
 }
