0f51884018f4abff4c23c1ec27303eb99a8d621a
0xFEEDC0DE64 <0xFEEDC0DE64@gmail.com>
2017-12-19 22:01:06 +0100

    Implemented WeatherWidget with API

--- plugins/weatherplugin/weatherwidget.h
+++ plugins/weatherplugin/weatherwidget.h
@@ -1,7 +1,10 @@
 #ifndef WEATHERWIDGET_H
 #define WEATHERWIDGET_H
 
+#include <memory>
+
 #include <QLabel>
+#include <QNetworkReply>
 
 class MainWindow;
 
@@ -12,8 +15,14 @@ class WeatherWidget : public QLabel
 public:
     explicit WeatherWidget(MainWindow &mainWindow);
 
+private Q_SLOTS:
+    void refresh();
+    void finished();
+
 private:
     MainWindow &m_mainWindow;
+
+    std::unique_ptr<QNetworkReply> m_reply;
 };
 
 #endif // WEATHERWIDGET_H
