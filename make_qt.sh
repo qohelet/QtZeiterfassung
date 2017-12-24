e04ef5cf1a018c8c655482f4c587e2835afefc1d
0xFEEDC0DE64 <0xFEEDC0DE64@gmail.com>
2017-12-24 20:52:11 +0100

    Removed caching in .travis.yml to avoid error

--- make_qt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash0
-git clone --branch=5.10 git://code.qt.io/qt/qt5.git
-pushd qt5
-perl init-repository --module-subset=qtbase,qtimageformats
-./configure -static -opensource -nomake examples -nomake tests -confirm-license
-make -j2
-sudo make install
-popd
